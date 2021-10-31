8b72da990f8799c59d00e2abbb3b4908704249f7
Tatsiana Kisialevich <t.kisialevich@gmail.com>
2021-10-31 19:48:26 +0300

    fix generalStage

--- src/redux/reducers/generalState.js
+++ src/redux/reducers/generalState.js
@@ -3,6 +3,8 @@ import productJSON from "../../components/product.json";
 const defaultState = {
   allproducts: productJSON,
   filteredProducts: [],
+  wishlist: [],
+  basket: [],
   type: "ALL",
 };
 
